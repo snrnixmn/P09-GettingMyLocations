720cad867b0c02533318079f777c22ef5ca00c60
18016339 <18016339@myrp.edu.sg>
2020-07-16 12:04:19 +0800

    Done

---
+++ app/src/main/java/com/example/p09_gettingmylocations/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.p09_gettingmylocations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnStartDetector, btnStopDetector, btnCheckRecords;
+    TextView tvLat, tvLong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStartDetector = findViewById(R.id.btnStartDetector);
+        btnStopDetector = findViewById(R.id.btnStopDetector);
+        btnCheckRecords = findViewById(R.id.btnCheckRecords);
+        tvLat = findViewById(R.id.tvLat);
+        tvLong = findViewById(R.id.tvLong);
+
+
+        btnStartDetector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                startService(i);
+            }
+        });
+
+        btnStopDetector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                stopService(i);
+            }
+        });
+
+    }
+}
